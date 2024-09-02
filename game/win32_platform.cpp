20686741357436e98145b7b191b3a70d15728e04
vishal-Nanaware <vishalnanaware7850@gmail.com>
2024-09-02 20:27:43 +0530

    Initialize BITMAPINFO structure for rendering buffer

--- game/win32_platform.cpp
+++ game/win32_platform.cpp
@@ -2,6 +2,10 @@
 #include <windows.h>
 
 boolean running = true;
+void* buffer_memeory;
+int buffer_width;
+int buffer_height;
+BITMAPINFO buffer_bitmap_info;
 // Define the Window Procedure function
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	LRESULT result = 0;
@@ -14,11 +18,23 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 		case WM_SIZE: {
 			RECT rect;
 			GetClientRect(hwnd, &rect);
-			int width = rect.left - rect.right;
-			int height = rect.top - rect.bottom;
-			int buffer = width * height * sizeof(unsigned int);
+			buffer_width = rect.right - rect.left;
+			buffer_height = rect.bottom - rect.top;
+			int buffer_size = buffer_width * buffer_height * sizeof(unsigned int);
 
+			if (buffer_memeory) VirtualFree(buffer_memeory, 0, MEM_RELEASE);
+
+			buffer_memeory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+
+			buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader);
+			buffer_bitmap_info.bmiHeader.biWidth = buffer_width;
+			buffer_bitmap_info.bmiHeader.biHeight = buffer_width;
+			buffer_bitmap_info.bmiHeader.biPlanes = 1;
+			buffer_bitmap_info.bmiHeader.biBitCount = 32;
+			buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
 		}break;
+
 		default: {
 			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
 		}
@@ -41,11 +57,26 @@ int APIENTRY WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdL
 		WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
 		1280, 720, 0, 0, hInstance,0);
 
-	while (running) {
-		MSG message;
-		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
-			TranslateMessage(&message);
-			DispatchMessage(&message);
-		}
-	}
+	HDC hdc = GetDC(window);
+
+		while (running) {
+			MSG message;
+			while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+				TranslateMessage(&message);
+				DispatchMessage(&message);
+			};
+
+			//simulate 
+			unsigned int* pixel = (unsigned int*)buffer_memeory;
+			for (int y = 0; y < buffer_height; y++) {
+				for (int x = 0; x < buffer_width; x++) {
+					*pixel++ = y*2;
+				}
+
+			}
+
+			//render
+
+			StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width, buffer_height, buffer_memeory,&buffer_bitmap_info, DIB_RGB_COLORS,SRCCOPY );
+		};
 };
