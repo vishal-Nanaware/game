f24f41544a1ba1e76cbc561d167b7ed2ebc81556
vishal-Nanaware <vishalnanaware7850@gmail.com>
2024-09-05 22:33:06 +0530

    fixing render_state BITMAPINFO

--- game/win32_platform.cpp
+++ game/win32_platform.cpp
@@ -2,12 +2,19 @@
 #include <windows.h>
 
 boolean running = true;
-void* buffer_memeory;
-int buffer_width;
-int buffer_height;
-BITMAPINFO buffer_bitmap_info;
+
+struct Render_state {
+	int height, width;
+	void* memory;
+	BITMAPINFO bitmap_info;
+
+};
+Render_state render_state;
+
+
 // Define the Window Procedure function
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
 	LRESULT result = 0;
 	switch (uMsg) {
 		case WM_CLOSE :
@@ -18,21 +25,21 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 		case WM_SIZE: {
 			RECT rect;
 			GetClientRect(hwnd, &rect);
-			buffer_width = rect.right - rect.left;
-			buffer_height = rect.bottom - rect.top;
-			int buffer_size = buffer_width * buffer_height * sizeof(unsigned int);
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
+			int size = render_state.height * render_state.width * sizeof(unsigned int);
 
-			if (buffer_memeory) VirtualFree(buffer_memeory, 0, MEM_RELEASE);
+			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
 
-			buffer_memeory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
 
-			buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader);
-			buffer_bitmap_info.bmiHeader.biWidth = buffer_width;
-			buffer_bitmap_info.bmiHeader.biHeight = buffer_width;
-			buffer_bitmap_info.bmiHeader.biPlanes = 1;
-			buffer_bitmap_info.bmiHeader.biBitCount = 32;
-			buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
+			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_info.bmiHeader.biHeight = render_state.width;
+			render_state.bitmap_info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
 		}break;
 
 		default: {
@@ -42,7 +49,7 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 	return result;
 }
 
-int APIENTRY WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,int nShowCmd) {
+int  WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,int nShowCmd) {
 
 	WNDCLASS window_class = {};
 	window_class.style = CS_HREDRAW | CS_VREDRAW;
@@ -67,16 +74,17 @@ int APIENTRY WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdL
 			};
 
 			//simulate 
-			unsigned int* pixel = (unsigned int*)buffer_memeory;
-			for (int y = 0; y < buffer_height; y++) {
-				for (int x = 0; x < buffer_width; x++) {
-					*pixel++ = y*2;
+			unsigned int* pixel = (unsigned int*)render_state.memory;
+			;
+			for (int y = 0; y < render_state.height; y++) {
+				for (int x = 0; x < render_state.width; x++) {
+					*pixel++ = 0X666666;
 				}
 
 			}
 
 			//render
 
-			StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width, buffer_height, buffer_memeory,&buffer_bitmap_info, DIB_RGB_COLORS,SRCCOPY );
+			StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory,&render_state.bitmap_info, DIB_RGB_COLORS,SRCCOPY );
 		};
 };
